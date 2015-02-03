2527bd370b99363f54f5580d135bf6d50244de96
JustasMi <justaaasm@gmail.com>
2015-02-03 11:01:57 +0000

    First changes

---
+++ Poker Training Tool/Classes/Card.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker_Training_Tool
+{
+    class Card
+    {
+        private String suit;
+        private String value;
+    }
+}
